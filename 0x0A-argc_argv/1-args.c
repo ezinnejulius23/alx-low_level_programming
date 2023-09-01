5831fdb0df14548663b62ecb4aa657371cf18bda
ezinnejulius23 <sussybabe01@yahoo.com>
2023-09-01 13:20:59 -0700

    Print number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	(void) argv;
+
+	return (0);
+
+}
