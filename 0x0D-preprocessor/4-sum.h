be739c52ba3edc8550d21b1b5e3771a3a2f463e8
ezinnejulius23 <sussybabe01@yahoo.com>
2023-09-11 13:00:01 -0700

    Number sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc:a header file that defines a function-like macro SUM(x, y)
+ *       that calculates the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
